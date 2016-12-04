d3fec6835fcf9102915aec4b7a261d74d03b8f3e
csar7248 <stefan.haberl@student.uibk.ac.at>
2016-12-04 18:45:54 +0100

    changed controller and readme

--- json_ld_importer/Classes/Controller/JSONLdTypoScriptController.php
+++ json_ld_importer/Classes/Controller/JSONLdTypoScriptController.php
@@ -5,8 +5,17 @@ use \TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class JSONLdTypoScriptController extends ActionController
 {
-    public function injectAction() {
+    public function injectAction(&$params) {
+        if(!isset($GLOBALS['TSFE']))
+            die("TSFE not set!");
 
+        $currentContent = $params['pObj']->content;
+        $currentInject = $GLOBALS['TSFE']->config['config']['jsonld'];
+
+        $newContent = str_replace('</head>', $currentInject . '</head>', $currentContent);
+        $params['pObj']->content = $newContent;
+
+        die('inside injectAction!');
     }
 
 
